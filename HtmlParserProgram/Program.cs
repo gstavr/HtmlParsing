52426ac4a8e95d54e57c89d32527c4fb00c734ff
gstavr1988@gmail.com <giorgos5756>
2018-08-27 17:40:20 +0300

    Add project files.

---
+++ HtmlParserProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HtmlParserProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        async static Task MainAsync(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("http://www.nzherald.co.nz/");
+            var pageContents = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(pageContents);
+            Console.ReadLine();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
